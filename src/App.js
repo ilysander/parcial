7d71a9938268284c4d63c06e5fe4532b7cdd752b
Felix Apaza <felix.apaza19@gmail.com>
2019-06-08 17:02:39 -0500

    primer avance

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ExamenPärcial from './componentes/ExamenParcial';
+
+function App() {
+  return (
+    <div className="App">
+      <ExamenPärcial />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/ExamenParcial.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ExamenPärcial = () =>  (
+    <div>
+        Mi examen parcial
+    </div>
+);
+
+export default ExamenPärcial;
